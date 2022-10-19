031ff1ea58fb86c5b80c027213b98660d98c1273
Esipov Aleksey <a_esipov@list.ru>
2022-10-19 11:00:11 +0300

    2. IndexControl. Таблица и вид. [#2092]

---
+++ src/main/java/ru/job4j/accidents/repository/AccidentMem.java
@@ -0,0 +1,22 @@
+package ru.job4j.accidents.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.job4j.accidents.model.Accident;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+@Repository
+public class AccidentMem {
+    private final HashMap<Integer, Accident> accidents = new HashMap<>();
+
+    public AccidentMem() {
+        accidents.put(1, new Accident(1, "name1", "text1", "address1"));
+        accidents.put(2, new Accident(2, "name2", "text2", "address2"));
+        accidents.put(3, new Accident(3, "name3", "text3", "address3"));
+    }
+
+    public Collection<Accident> findAll() {
+        return accidents.values();
+    }
+}
---
+++ src/main/java/ru/job4j/accidents/service/AccidentService.java
@@ -0,0 +1,21 @@
+package ru.job4j.accidents.service;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Service;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.repository.AccidentMem;
+import java.util.Collection;
+
+@ThreadSafe
+@Service
+public class AccidentService {
+    private final AccidentMem accidentMem;
+
+    public AccidentService(AccidentMem accidentMem) {
+        this.accidentMem = accidentMem;
+    }
+
+    public Collection<Accident> findAll() {
+        return accidentMem.findAll();
+    }
+}
