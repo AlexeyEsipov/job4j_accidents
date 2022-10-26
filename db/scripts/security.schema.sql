4897f369e41509b19af9bbec203fee886a07969e
Esipov Aleksey <a_esipov@list.ru>
2022-10-26 21:08:54 +0300

    2. Регистрация пользователя [#296069]

--- db/scripts/security.schema.sql
+++ db/scripts/security.schema.sql
@@ -1,12 +1,19 @@
-CREATE TABLE IF NOT EXISTS users (
-    username VARCHAR NOT NULL,
-    password VARCHAR NOT NULL,
-    enabled boolean default true,
-    PRIMARY KEY (username)
+CREATE TABLE authorities (
+    id serial primary key,
+    authority VARCHAR NOT NULL unique
 );
 
-CREATE TABLE IF NOT EXISTS authorities (
-    username VARCHAR NOT NULL,
-    authority VARCHAR NOT NULL,
-    FOREIGN KEY (username) REFERENCES users(username)
+CREATE TABLE users (
+    id serial primary key,
+    username VARCHAR NOT NULL unique,
+    password VARCHAR NOT NULL,
+    enabled boolean default true,
+    authority_id int not null references authorities(id)
 );
+
+insert into authorities (authority) values ('ROLE_USER');
+insert into authorities (authority) values ('ROLE_ADMIN');
+
+insert into users (username, enabled, password, authority_id)
+values ('root', true, '$2a$10$wY1twJhMQjGVxv4y5dBC5ucCBlzkzT4FIGa4FNB/pS9GaXC2wm9/W',
+        (select id from authorities where authority = 'ROLE_ADMIN'));
---
+++ src/main/java/ru/job4j/accidents/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package ru.job4j.accidents.repository;
+import org.springframework.data.repository.CrudRepository;
+import ru.job4j.accidents.model.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
