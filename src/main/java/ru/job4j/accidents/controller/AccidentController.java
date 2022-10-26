5eb82e83370cfe0abb0dc682352557a8bd3505c0
Esipov Aleksey <a_esipov@list.ru>
2022-10-26 13:08:05 +0300

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -2,22 +2,23 @@ package ru.job4j.accidents.controller;
 
 import lombok.AllArgsConstructor;
 import net.jcip.annotations.ThreadSafe;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import java.util.Collection;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashSet;
 import java.util.Set;
-import javax.servlet.http.HttpServletRequest;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.service.AccidentService;
 import ru.job4j.accidents.service.AccidentTypeService;
 import ru.job4j.accidents.service.RuleService;
+
 
 @Controller
 @ThreadSafe
@@ -29,29 +30,21 @@ public class AccidentController {
 
     @GetMapping("/accidents")
     public String accidents(Model model) {
-        model.addAttribute("user", "Alex");
-        model.addAttribute("accidents", accidentService.findAll());
+        model.addAttribute("user", SecurityContextHolder.getContext()
+                                                .getAuthentication().getPrincipal())
+            .addAttribute("accidents", accidentService.findAll());
         return "accidents";
     }
 
     @GetMapping("/addAccident")
     public String viewCreateAccident(Model model) {
-        model.addAttribute("user", "Alex");
-        Collection<AccidentType> types = accidentTypeService.findAll();
-        Collection<Rule> rules = ruleService.findAll();
-        model.addAttribute("types", types);
-        model.addAttribute("rules", rules);
+        fillModel(model);
         return "createAccident";
     }
 
     @GetMapping("/formUpdateAccident")
     public String update(@RequestParam("id") int id, Model model) {
-        model.addAttribute("user", "Alex");
-        Collection<AccidentType> types = accidentTypeService.findAll();
-        Collection<Rule> rules = ruleService.findAll();
-        model.addAttribute("types", types);
-        model.addAttribute("accident", accidentService.findById(id));
-        model.addAttribute("rules", rules);
+        fillModel(model).addAttribute("accident", accidentService.findById(id));
         return "editAccident";
     }
 
@@ -59,15 +52,7 @@ public class AccidentController {
     public String update(@ModelAttribute Accident accident,
                          @RequestParam("type.id") int id,
                          HttpServletRequest req) {
-        AccidentType type = accidentTypeService.findById(id);
-        String[] ids = req.getParameterValues("rIds");
-        Set<Rule> rules = new HashSet<>();
-        for (String statId : ids) {
-            rules.add(ruleService.findById(Integer.parseInt(statId)));
-        }
-        accident.setType(type);
-        accident.setRules(rules);
-        accidentService.update(accident);
+        accidentService.update(fillAccident(accident, id, req));
         return "redirect:/accidents";
     }
 
@@ -75,6 +60,11 @@ public class AccidentController {
     public String save(@ModelAttribute Accident accident,
                        @RequestParam("type.id") int id,
                        HttpServletRequest req) {
+        accidentService.add(fillAccident(accident, id, req));
+        return "redirect:/accidents";
+    }
+
+    private Accident fillAccident(Accident accident, int id, HttpServletRequest req) {
         AccidentType type = accidentTypeService.findById(id);
         String[] ids = req.getParameterValues("rIds");
         Set<Rule> rules = new HashSet<>();
@@ -83,7 +73,14 @@ public class AccidentController {
         }
         accident.setType(type);
         accident.setRules(rules);
-        accidentService.add(accident);
-        return "redirect:/accidents";
+        return accident;
+    }
+
+    private Model fillModel(Model model) {
+        model.addAttribute("user", SecurityContextHolder.getContext()
+                                                .getAuthentication().getPrincipal())
+            .addAttribute("types", accidentTypeService.findAll())
+            .addAttribute("rules", ruleService.findAll());
+        return model;
     }
 }
