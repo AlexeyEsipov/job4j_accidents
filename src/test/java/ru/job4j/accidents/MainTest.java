5f294bc71a2f07f98783f80a70c047fb3e4c18d8
Esipov Aleksey <a_esipov@list.ru>
2023-04-30 10:04:17 +0300

    Добавил MainTest

---
+++ src/test/java/ru/job4j/accidents/MainTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.accidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+@SpringBootTest
+class MainTest {
+    @Test
+    void contextLoads() {
+    }
+}
